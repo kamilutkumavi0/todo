1e6320b3a61f974cbc19f04fa656f364def02c5e
kamilutkumavi0 <kamilutkumavi0@gmail.com>
2024-03-28 15:09:21 +0300

    yeni

--- todo-output/src/lib.rs
+++ todo-output/src/lib.rs
@@ -1,20 +1,22 @@
 use todo_structure::{todo::{Todo, TodoStatus},date::TodoDate};
+use colored::Colorize;
+
 pub fn todo_print(todo: Todo){
     match todo.status{
-        Some(TodoStatus::NotStarted) => print!("- "),
-        Some(TodoStatus::Continue) => print!("! "),
-        Some(TodoStatus::Done) => print!("+ "),
+        Some(TodoStatus::NotStarted) => print!("{} ", "-".red()),
+        Some(TodoStatus::Continue) => print!("{} ", "!".yellow()),
+        Some(TodoStatus::Done) => print!("{} ", "+".green()),
         None => print!("  "),
     }
-    print!("{} ", todo.name);
+    print!("{} ", todo.name.bold());
 
     let description = match todo.description{
                             Some(a) => a,
                             None => String::new(),
                     };
-    print!("{} ", description);
+    print!("{} ", description.bright_blue());
     match todo.start_date{
-        Some(date) => print!("{} ", date),
+        Some(date) => print!("{} -> ", date),
         None => print!(" "),
     };
     match todo.finish_date{
